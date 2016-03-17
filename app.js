370a036ff224525a80c5fc3c49857eb2d60007c2
Kaustav <kaustv.datta@gmail.com>
2016-03-17 21:19:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,25 @@ app.directive('stopwatch', function () {
 		controller: function () {
 			var self = this;
 			self.states = ['ready', 'inprogress', 'stopped'];
+			self.startTime = 0;
+			self.totalDuration = 0;
+			self.intervalId = undefined;
 			
 			self.log = function (data) {
 				console.log(data);
+			};
+			
+			self.start = function () {
+				self.startTime = Date.now();
+				self.intervalId = window.setInterval(self.startWatch, 4);
+			};
+			
+			self.startWatch = function () {
+				self.totalDuration = Date.now() - self.startTime;
+			};
+			
+			self.stop = function () {
+				window.clearInterval(self.intervalId);
 			};
 		},
 		bindToController: true,
